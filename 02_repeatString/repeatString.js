1d064e4eaebb8fccb23de9a328b69c6a64113d45
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-20 11:20:22 +1100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let newString = string;
+  for (let x = 1; x < num; x++) {
+    newString = newString + string;
+  }
+  if (num === 0) {
+    newString = "";
+  }
+  if (num < 0) {
+    newString = "ERROR";
+  }
+  return newString;
 };
 
 // Do not edit below this line

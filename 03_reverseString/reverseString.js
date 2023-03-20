6ae2be2d5a720622a81b2cb150dffbbd4766b87b
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-21 00:13:20 +1100

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  // Take the input string and shove into an array
+  let charArray = Array.from(word);
+  let charArrayReverse = charArray.reverse();
+  let reverseString = charArrayReverse.join("");
+  return reverseString;
+  // return backwards string
 };
 
 // Do not edit below this line

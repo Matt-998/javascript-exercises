9a977441c542432c15a7ec80a2d78cf542506eb6
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-22 11:29:39 +1100

    04_removeFromArray - Start to piece things together

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, x) {
+  console.log(array);
+  let index = array.indexOf(x);
+  console.log(index);
+  let f = array.splice(index, 1);
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line

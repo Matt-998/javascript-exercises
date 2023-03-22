8813774fc0f05b73ecc2a13705f1b7c9e164918d
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-23 09:22:23 +1100

    04_removeFromArray - Add if else to pass all tests
    My solution ended up being a bit more complex than the supplied solution
    but i'm pleased a managed to piece together somthing on my own.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,11 +2,13 @@ const removeFromArray = function (array, ...theArgs) {
   const args = theArgs;
   let iterator = args.values();
   for (let remove of iterator) {
-    let index = array.indexOf(remove);
-    array.splice(index, 1);
+    if (array.indexOf(remove) === -1) {
+      continue;
+    } else {
+      let index = array.indexOf(remove);
+      array.splice(index, 1);
+    }
   }
-  console.log(array);
-
   return array;
 };
 

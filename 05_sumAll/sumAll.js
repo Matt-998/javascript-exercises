73b148870ee90912356e375ea793e4cb9092794a
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-23 11:23:59 +1100

    05_sumAll Pass 4/5 tests
    Solution kind of bad so far but still works

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  let x = firstNum;
+  let y = secondNum;
+  const array = [];
+  if (x > y) {
+    let temp = x;
+    x = y;
+    y = temp;
+  } else if (x < 0 || y < 0 || x === NaN || y === NaN) {
+    return "ERROR";
+  }
+  for (let n = x; n <= y; n++) {
+    array.push(n);
+  }
+  let initial = 0;
+  let sum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initial
+  );
+  return sum;
 };
 
 // Do not edit below this line

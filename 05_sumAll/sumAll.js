3638efa4036955655d67045013ac3dd0aa3e1af7
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-03-25 13:33:04 +1100

    Complete 05_sumAll
    My solution is only a bit more complex than the provided solution which
    i'm pleased with. No need for an array apparently which is fair.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,22 @@
 const sumAll = function (firstNum, secondNum) {
+  // Global variables for arguments and empty array
   let x = firstNum;
   let y = secondNum;
   const array = [];
+  // Reverse 'firstNum' and 'secondNum' arguments if 'firstNum' is greater
   if (x > y) {
     let temp = x;
     x = y;
     y = temp;
-  } else if (x < 0 || y < 0 || x === NaN || y === NaN) {
+    // Checks for negative inputs or non integer inputs
+  } else if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
     return "ERROR";
   }
+  // Constructs array from inputs
   for (let n = x; n <= y; n++) {
     array.push(n);
   }
+  // Sum up completed array and return sum
   let initial = 0;
   let sum = array.reduce(
     (accumulator, currentValue) => accumulator + currentValue,

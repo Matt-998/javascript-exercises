82ecd0e920eacaa4bf465e2415337d003906a0ea
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-25 16:15:52 +1000

    Complete 09_palindromes
    Managed to get a solution on my own after some googling. After checking
    the solution i did refine my solution to be more concise whilst still
    being easily readable

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reverseString = newString.split("").reverse().join("");
+  if (reverseString === newString) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line

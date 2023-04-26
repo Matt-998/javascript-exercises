befc5ce502fcc540c68329ae8deef59caa1cb03e
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-26 23:54:31 +1000

    Complete 10_fibonacci
    I wasn't clever enough to come up with the simple algorithm for this
    exercise, i had to be handed it. Maths is not my strong suit.
    Not too disappointed about it though since once provided with the
    algorith it was simple to pass all the tests. I'm here to learn how to
    code not how to maths.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let num1 = 0,
+    num2 = 1,
+    temp;
+  if (num < 0) {
+    return "OOPS";
+  }
+  for (let i = 1; i < num; i++) {
+    temp = num2;
+    num2 = num1 + num2;
+    num1 = temp;
+  }
+  return num2;
 };
 
 // Do not edit below this line

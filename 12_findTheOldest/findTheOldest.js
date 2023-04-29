7aef17f8f97f9dc04748122f049411ad29b46d9f
Matt-998 <123720140+Matt-998@users.noreply.github.com>
2023-04-29 16:18:15 +1000

    Complete 12_findTheOldest
    This one was pretty tough, spent a few hours on it. In the end i was
    able to come up with a decent solution then condense it down with my
    first uses of ternary operators. I honestly much prefer my solution to
    the provided one. Don't know if it's worse or not though.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  const year = new Date().getFullYear();
+  const oldest = array.sort((a, b) => {
+    if (a.yearOfDeath === undefined) {
+      return year - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? 1 : -1;
+    } else if (b.yearOfDeath === undefined) {
+      return a.yearOfDeath - a.yearOfBirth < year - b.yearOfBirth ? 1 : -1;
+    } else {
+      return a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth
+        ? 1
+        : -1;
+    }
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
